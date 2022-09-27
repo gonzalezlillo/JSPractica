8b8e054d0dc720324cc76a3291a5359c0ef10927
QukkiQ <oscar.zapata.g@mail.pucv.cl>
2022-09-26 22:49:23 -0300

    Estructuracion Basica

---
+++ Assets/JS/JSPractica.js
@@ -0,0 +1,28 @@
+var formulario = document.getElementById("formulario");
+formulario.addEventListener('submit',enviar);
+
+function enviar(){
+    
+    var email = document.querySelector(".asunto");
+    var correo = document.querySelector(".correo");
+    var clave = document.querySelector(".clave");
+    alert(`Hola ${nombre.value}, tu correo electrónico es:
+    ${correo.value} y tu clave es ${clave.value}, Bienvenido`);
+};
+
+
+let validacionNombre = () => {
+    do {
+        var nombre = document.querySelector(".nombre");
+        var expRegNombre=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+        
+        if(nombre.match(expRegNombre)) {
+            alert('Nombre ingresado correctamente.');
+        }
+        else {
+            alert('Nombre invalido.');      
+        }
+
+    }while (!nombre.match(expRegNombre));
+    return nombre; 
+}

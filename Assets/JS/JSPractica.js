5ffdc2c7e693571aea12006b9724774392937539
gonzalezlillo <gonzalezlillo@yahoo.com>
2022-09-28 00:39:23 -0300

    OK validaciones en JS del ej.1

--- Assets/JS/JSPractica.js
+++ Assets/JS/JSPractica.js
@@ -1,35 +1,103 @@
 const expRegCorreo =/^\w+@(\w+\.)+\w{2,4}$/;
 const expRegNombre=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
 
-var formulario = document.getElementById("formulario");
+const formulario = document.getElementById("formulario");
 formulario.addEventListener('submit',enviar);
 
+const nombre = document.querySelector("#nombre");
+const asunto = document.querySelector("#asunto");
+const email = document.querySelector("#email");
+const mensaje = document.querySelector("#mensaje");
 
-function enviar(){
-    
-    var email = document.querySelector(".asunto");
-    var correo = document.querySelector(".correo");
-    var clave = document.querySelector(".clave");
-    alert(`Hola ${nombre.value}, tu correo electrónico es:
-    ${correo.value} y tu clave es ${clave.value}, Bienvenido`);
+const validacion_nombre = document.getElementById("validacion_nombre");
+const validacion_correo = document.getElementById("validacion_correo");
+const validacion_asunto = document.getElementById("validacion_asunto");
+const validacion_mensaje = document.getElementById("validacion_mensaje");
+
+const resultado = document.getElementById("resultado");
+
+
+function enviar(event){
+
+   event.preventDefault();
+
+   let isValidMessage = validacionMensaje();
+   let isValidSubject = validacionAsunto();
+   let isValidMail = validacionCorreo();
+   let isValidName = validacionNombre();
+
+   if (isValidName && isValidMail && isValidSubject && isValidMessage) {
+      resultado.innerText = "¡Mensaje enviado exitosamente!";
+   }
 };
 
 
 let validacionNombre = () => {
-    var nombre = document.getElementById("nombre");
-    
-    if(!nombre.value)
-     {
-      alert("El campo nombre es requerido");
+   
+   if(!nombre.value) {
+      resultado.innerText = "";
+      validacion_nombre.innerText =  "El nombre es requerido";
       nombre.focus();
       return false;
-     }
-     if (!expRegNombre.exec(nombre.value))
-     {
-        alert("El campo nombre solo admite letras y espacios.")
-        nombre.focus();
-        return false;
-     }
+   } else {
+      validacion_nombre.innerText =  "";
+   }
 
-    return nombre; 
+   if (!expRegNombre.exec(nombre.value)) {
+      resultado.innerText = "";
+      validacion_nombre.innerText = "El nombre solo admite letras y espacios";
+      nombre.focus();
+      return false;
+   } else {
+      return true; 
+   }
+
+    
+}
+
+let validacionCorreo = () => {
+   
+   if(!email.value) {
+      resultado.innerText = "";
+      validacion_correo.innerText =  "El correo es requerido";
+      email.focus();
+      return false;
+   } else {
+      validacion_correo.innerText =  "";
+   }
+
+   if (!expRegCorreo.exec(email.value)) {
+      resultado.innerText = "";
+      validacion_correo.innerText = "El correo tiene un formato incorrecto";
+      email.focus();
+      return false;
+   } else {
+      return true; 
+   }
+}
+
+let validacionAsunto = () => {
+   
+   if(!asunto.value) {
+      resultado.innerText = "";
+      validacion_asunto.innerText =  "El asunto es requerido";
+      asunto.focus();
+      return false;
+   } else {
+      validacion_asunto.innerText =  "";
+      return true;
+   }
+}
+
+let validacionMensaje = () => {
+   
+   if(!mensaje.value) {
+      resultado.innerText = "";
+      validacion_mensaje.innerText =  "El mensaje es requerido";
+      mensaje.focus();
+      return false;
+   } else {
+      validacion_mensaje.innerText =  "";
+      return true;
+   }
 }
